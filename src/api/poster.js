1bc785249c39d2f76b19ed8f79f75cd2dd81f590
Nissim Lavy <nissim.lavy@talkspace.com>
2019-08-06 14:31:31 -0400

    Change poster endpoint to accept title instead

--- src/api/poster.js
+++ src/api/poster.js
@@ -1,12 +1,17 @@
-'use strict'
+'use strict';
 
-const Wreck = require('@hapi/wreck')
-const Joi = require('@hapi/joi')
+const Wreck = require('@hapi/wreck');
+const Joi = require('@hapi/joi');
 
-let posterCall = async (api, id) => {
-  const { req, res, payload } = await Wreck.get(`http://img.omdbapi.com/?apikey=${api}&i=${id}`)
-  return payload
-}
+let posterCall = async (api, title) => {
+  const { moviereq, movieres, payload: moviePayload } = await Wreck.get(
+    `http://www.omdbapi.com/?apikey=${api}&t=${title}`
+  );
+  const { req, res, payload } = await Wreck.get(
+    `http://img.omdbapi.com/?apikey=${api}&i=${moviePayload.imdbID}`
+  );
+  return payload;
+};
 
 const plugin = {
   name: 'poster',
@@ -14,25 +19,25 @@ const plugin = {
   register: (server, options) => {
     server.route({
       method: ['GET', 'PUT', 'POST'],
-      path: '/api/poster/{id?}',
+      path: '/api/poster/{title?}',
       config: {
         validate: {
           params: {
-            id: Joi.string().min(9).max(10).required()
-          }
-        }
+            title: Joi.string().required(),
+          },
+        },
       },
       handler: async (request, h) => {
-        let findPoster
+        let findPoster;
         try {
-          findPoster = await posterCall(process.env.API_KEY, request.params.id)
+          findPoster = await posterCall(process.env.API_KEY, request.params.title);
         } catch (err) {
-          console.error(err)
+          console.error(err);
         }
-        return h.response(findPoster).type('image/jpeg')
-      }
-    })
-  }
-}
+        return h.response(findPoster).type('image/jpeg');
+      },
+    });
+  },
+};
 
-module.exports = plugin
+module.exports = plugin;
