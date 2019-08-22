b2c27256378567e4ab9e62b078659d89450c0260
Juan Pablo Zuluaga <juanpazup@gmail.com>
2019-08-22 16:44:40 -0500

    use find movie call to get the id and look for the poster

--- src/api/poster.js
+++ src/api/poster.js
@@ -3,6 +3,11 @@
 const Wreck = require('@hapi/wreck')
 const Joi = require('@hapi/joi')
 
+let movieCall = async (key, title) => {
+  const { req, res, payload } = await Wreck.get(`http://www.omdbapi.com/?apikey=${key}&t=${title}`)
+  return payload
+}
+
 let posterCall = async (api, id) => {
   const { req, res, payload } = await Wreck.get(`http://img.omdbapi.com/?apikey=${api}&i=${id}`)
   return payload
@@ -14,18 +19,20 @@ const plugin = {
   register: (server, options) => {
     server.route({
       method: ['GET', 'PUT', 'POST'],
-      path: '/api/poster/{id?}',
+      path: '/api/poster/{title?}',
       config: {
         validate: {
           params: {
-            id: Joi.string().min(9).max(10).required()
+            title: Joi.string().required()
           }
         }
       },
       handler: async (request, h) => {
         let findPoster
         try {
-          findPoster = await posterCall(process.env.API_KEY, request.params.id)
+          let response = await movieCall(process.env.API_KEY, request.params.title)
+          let { imdbID } = JSON.parse(response.toString());
+          findPoster = await posterCall(process.env.API_KEY, imdbID)
         } catch (err) {
           console.error(err)
         }
