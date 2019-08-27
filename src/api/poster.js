9df248f1113f5ee016f1c8580d6370b639343a77
Alexander Ochoa <ochoaseguralex@gmail.com>
2019-08-27 14:38:24 -0500

    Poster implementation on name

--- src/api/poster.js
+++ src/api/poster.js
@@ -3,6 +3,11 @@
 const Wreck = require('@hapi/wreck')
 const Joi = require('@hapi/joi')
 
+let movieCall = async (key, title) => {
+  const { req, res, payload } = await Wreck.get(`http://www.omdbapi.com/?apikey=${key}&t=${title}`)
+  return payload
+}
+
 let posterCall = async (api, id) => {
   const { req, res, payload } = await Wreck.get(`http://img.omdbapi.com/?apikey=${api}&i=${id}`)
   return payload
@@ -14,18 +19,22 @@ const plugin = {
   register: (server, options) => {
     server.route({
       method: ['GET', 'PUT', 'POST'],
-      path: '/api/poster/{id?}',
+      path: '/api/poster/{title?}',
       config: {
         validate: {
           params: {
-            id: Joi.string().min(9).max(10).required()
+            title: Joi.string().required()
           }
         }
       },
       handler: async (request, h) => {
+        let findMovie
         let findPoster
         try {
-          findPoster = await posterCall(process.env.API_KEY, request.params.id)
+          findMovie = JSON.parse(await movieCall(process.env.API_KEY, request.params.title));
+          const movieId = findMovie.imdbID;
+
+          findPoster = await posterCall(process.env.API_KEY, movieId)
         } catch (err) {
           console.error(err)
         }
