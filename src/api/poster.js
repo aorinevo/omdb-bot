903f432011c98cd94058b14be736252423f9b664
Diego Reyes <reyesdiego@hotmail.com>
2019-10-31 11:53:59 -0300

    Changing /api/poster/{id?} endpoint in order to get imdbID or movie name in the id param

--- src/api/poster.js
+++ src/api/poster.js
@@ -1,38 +1,51 @@
-'use strict'
+"use strict";
 
-const Wreck = require('@hapi/wreck')
-const Joi = require('@hapi/joi')
+const Wreck = require("@hapi/wreck");
+const Joi = require("@hapi/joi");
 
 let posterCall = async (api, id) => {
-  const { req, res, payload } = await Wreck.get(`http://img.omdbapi.com/?apikey=${api}&i=${id}`)
-  return payload
-}
+  const { req, res, payload } = await Wreck.get(
+    `http://img.omdbapi.com/?apikey=${api}&i=${id}`
+  );
+  return payload;
+};
 
 const plugin = {
-  name: 'poster',
-  version: '0.1.0',
+  name: "poster",
+  version: "0.1.0",
   register: (server, options) => {
     server.route({
-      method: ['GET', 'PUT', 'POST'],
-      path: '/api/poster/{id?}',
+      method: ["GET", "PUT", "POST"],
+      path: "/api/poster/{id?}",
       config: {
         validate: {
           params: {
-            id: Joi.string().min(9).max(10).required()
+            id: Joi.string().required()
           }
         }
       },
       handler: async (request, h) => {
-        let findPoster
+        let findPoster;
+        let paramId = request.params.id;
         try {
-          findPoster = await posterCall(process.env.API_KEY, request.params.id)
-        } catch (err) {
-          console.error(err)
-        }
-        return h.response(findPoster).type('image/jpeg')
-      }
-    })
-  }
-}
+          const { req, res, payload } = await Wreck.get(
+            `http://www.omdbapi.com/?apikey=${
+              process.env.API_KEY
+            }&t=${request.params.id.toString()}`
+          );
 
-module.exports = plugin
+          if (payload) {
+            paramId = JSON.parse(payload.toString())["imdbID"];
+          }
+
+          findPoster = await posterCall(process.env.API_KEY, paramId);
+        } catch (err) {
+          console.error(err);
+        }
+        return h.response(findPoster).type("image/jpeg");
+      }
+    });
+  }
+};
+
+module.exports = plugin;
