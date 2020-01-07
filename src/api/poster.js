251d743858cd867ae84363fbaa42adf3deb629e1
Louis Rowan <louis@Louiss-MacBook-Pro.local>
2020-01-07 17:53:18 -0500

    updates poster route to accept movie title

--- src/api/poster.js
+++ src/api/poster.js
@@ -8,26 +8,49 @@ let posterCall = async (api, id) => {
   return payload
 }
 
+let movieCall = async (key, title) => {
+  const { req, res, payload } = await Wreck.get(`http://www.omdbapi.com/?apikey=${key}&t=${title}`)
+  return payload
+}
+
 const plugin = {
   name: 'poster',
   version: '0.1.0',
   register: (server, options) => {
     server.route({
-      method: ['GET', 'PUT', 'POST'],
-      path: '/api/poster/{id?}',
+      method: 'GET',
+      path: '/api/poster/{title?}',
       config: {
         validate: {
           params: {
-            id: Joi.string().min(9).max(10).required()
+            title: Joi.string().required()
           }
         }
       },
       handler: async (request, h) => {
-        let findPoster
+
+        let findMovie
+        let imdbID;
+        let findPoster;
+
         try {
-          findPoster = await posterCall(process.env.API_KEY, request.params.id)
+          console.log('call find movie')
+          findMovie = await movieCall(process.env.API_KEY, request.params.title)
+          const stringified = findMovie.toString()
+          console.log(stringified)
+
+          const jsonified = JSON.parse(stringified)
+
+          console.log(jsonified)
+
+          imdbID = jsonified.imdbID;
+
+          console.log('imdbId', imdbID)
+          findPoster = await posterCall(process.env.API_KEY, imdbID)
+          console.log('findPoster res', findPoster)
         } catch (err) {
           console.error(err)
+          return h.response(err);
         }
         return h.response(findPoster).type('image/jpeg')
       }
